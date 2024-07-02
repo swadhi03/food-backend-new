9b497945a5bfa044f7e7a6e53c1bfa4221b471a6
swadhi03 <swathiksanthosh03@gmail.com>
2024-07-02 14:41:39 +0530

    completed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const { usermodel } = require("./models/user")
-const {foodmodel} =require("./model/Add")
+const { foodmodel } = require("./model/Add")
 
 const app = express()
 app.use(cors())
@@ -21,84 +21,85 @@ app.post("/signup", async (req, res) => {
     let input = req.body
     let hashedPassword = await generateHashedPassword(input.password)
     console.log(hashedPassword)
-    input.password=hashedPassword
+    input.password = hashedPassword
     let user = new usermodel(input)
     user.save()
     console.log(user)
-    res.json({"status":"success"})
+    res.json({ "status": "success" })
 })
 
-app.post("/signin", (req, res)=>{
+app.post("/signin", (req, res) => {
     let input = req.body
-    usermodel.find({"email": req.body.email}).then(
-        (response)=>{
-            if (response.length>0) {
-               let dbPassword = response[0].password
-               console.log(dbPassword)
-               bcrypt.compare(input.password, dbPassword, (error, isMatch)=>{
-                if(isMatch){
-                    jwt.sign({email: input.email}, "food-app", {expiresIn: "1d"}, 
-                        (error, token)=>{
-                            if(error) {
-                                res.json({"status":"Unable to create token"})
+    usermodel.find({ "email": req.body.email }).then(
+        (response) => {
+            if (response.length > 0) {
+                let dbPassword = response[0].password
+                console.log(dbPassword)
+                bcrypt.compare(input.password, dbPassword, (error, isMatch) => {
+                    if (isMatch) {
+                        jwt.sign({ email: input.email }, "food-app", { expiresIn: "1d" },
+                            (error, token) => {
+                                if (error) {
+                                    res.json({ "status": "Unable to create token" })
+                                }
+                                else {
+                                    res.json({ "status": "success", "userId": response[0]._id, "token": token })
+                                }
                             }
-                            else {
-                                res.json({"status":"success", "userId": response[0]._id, "token":token})
-                            }
-                        }
-                    )
-                }
-                else {
-                    res.json({"status":"Incorrect Password"})
-                }
-               }) 
+                        )
+                    }
+                    else {
+                        res.json({ "status": "Incorrect Password" })
+                    }
+                })
             } else {
-                res.json({"status":"User not found"})
+                res.json({ "status": "User not found" })
             }
         }
     )
 })
 
-app.post("/add", (req,res)=>{
-    let input=req.body
-    let food=new foodmodel(input)
+
+app.post("/add", (req, res) => {
+    let input = req.body
+    let food = new foodmodel(input)
     food.save()
     console.log(food)
-    res.json({"status":"success"})
+    res.json({ "status": "success" })
 })
 
-app.post("/search", (req,res)=>{
-    let input=req.body
+app.post("/search", (req, res) => {
+    let input = req.body
     foodmodel.find(input).then(
-        data=>{
+        data => {
             res.json(data)
         }
-        ).catch(
-            error=>{
-                res.json(error)
-            }
-        )
-})
-app.post("/foodview",(req,res)=>{
-    foodmodel.find().then((data)=>{
-        res.json(data)
-    }).catch(
-        (error)=>{
+    ).catch(
+        error => {
             res.json(error)
         }
     )
-    })
-    app.post("/cart",(req,res)=>{
-        foodmodel.find().then((data)=>{
-            res.json(data)
-        }).catch(
-            (error)=>{
-                res.json(error)
-            }
-        )
-        })
-        
+})
+app.post("/foodview", (req, res) => {
+    foodmodel.find().then((data) => {
+        res.json(data)
+    }).catch(
+        (error) => {
+            res.json(error)
+        }
+    )
+})
+app.post("/cart", (req, res) => {
+    foodmodel.find().then((data) => {
+        res.json(data)
+    }).catch(
+        (error) => {
+            res.json(error)
+        }
+    )
+})
 
-app.listen(8088,()=>{
+
+app.listen(8088, () => {
     console.log("Server Started")
 })
